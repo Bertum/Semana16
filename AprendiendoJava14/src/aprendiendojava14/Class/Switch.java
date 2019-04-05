8ae8fdd65ec4aad16214b6aca015926cb733604a
Bertum <albertodelcru@gmail.com>
2019-04-06 00:20:56 +0200

    Triggers class

---
+++ AprendiendoJava14/src/aprendiendojava14/Class/Switch.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aprendiendojava14.Class;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Bertum
+ */
+public class Switch extends Trigger {
+
+    private Door target;
+
+    public Switch(int x, int y, BufferedImage spriteOff, BufferedImage spriteOn, Door target) {
+        super(x, y, spriteOff, spriteOn);
+        this.target = target;
+    }
+
+    public void trigger(Graphics2D interactiveCtx) {
+        this.triggered = !this.triggered;
+        if (this.triggered) {
+            this.currentSprite = this.spriteOn;
+        } else {
+            this.currentSprite = this.spriteOff;
+        }
+        this.draw(interactiveCtx);
+        this.target.trigger();
+    }
+}
---
+++ AprendiendoJava14/src/aprendiendojava14/Class/Trigger.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aprendiendojava14.Class;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Bertum
+ */
+public class Trigger {
+
+    public int x;
+    public int y;
+    public BufferedImage spriteOff;
+    public BufferedImage spriteOn;
+    public BufferedImage currentSprite;
+    public boolean triggered;
+
+    public Trigger(int x, int y, BufferedImage spriteOff, BufferedImage spriteOn) {
+        this.x = x;
+        this.y = y;
+        this.spriteOff = spriteOff;
+        this.spriteOn = spriteOn;
+        this.triggered = false;
+        this.currentSprite = spriteOff;
+    }
+
+    public void trigger() {
+        this.triggered = !this.triggered;
+        if (this.triggered) {
+            this.currentSprite = this.spriteOn;
+        } else {
+            this.currentSprite = this.spriteOff;
+        }
+    }
+
+    public void draw(Graphics2D interactiveCtx) {
+        interactiveCtx.drawImage(this.currentSprite, this.x, this.y, null);
+    }
+}
