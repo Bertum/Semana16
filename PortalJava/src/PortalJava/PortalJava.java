9d0d68d7914a22f3c08de090d49cec2461d1d717
Bertum <albertodelcru@gmail.com>
2019-04-06 13:49:19 +0200

    Load images in main

--- PortalJava/src/PortalJava/PortalJava.java
+++ PortalJava/src/PortalJava/PortalJava.java
@@ -1,13 +1,14 @@
 package PortalJava;
 
+import PortalJava.Class.Door;
+import PortalJava.Class.Player;
+import PortalJava.Class.Switch;
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,49 +17,99 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class PortalJava implements Runnable {
+    //Canvas dimensions
 
-    // DECLARACION DE CONDICIONES INICIALES
-    JFrame frame;                                                               // Creacion de un marco sobre el que trabajar
-    Canvas canvas;                                                              // Creacion de un canvas
+    int CANVAS_WIDTH = 768;
+    int CANVAS_HEIGHT = 576;
+
+    // Contextos
+    Graphics2D MAP_CTX = null;
+    Graphics2D COLLISION_CTX = null;
+    Graphics2D INTERACTIVE_CTX = null;
+    Graphics2D SCANNERS_CTX = null;
+
+    float timer;
+    Player player = null;
+//Image
+    BufferedImage closeDoorImageV = null;
+    BufferedImage openDoorImageV = null;
+    BufferedImage switchOffImage = null;
+    BufferedImage switchOnImage = null;
+
+//Switch
+    Switch switchButton = null;
+    Door door = null;
+
+//Images canvas
+    BufferedImage imgMap = null;
+    BufferedImage imgMapCollisions = null;
+
+    JFrame frame;
+    Canvas canvas;
+    Canvas interactionCanvas;
     BufferStrategy bufferStrategy;
     private BufferedImage imagenmanzana;
     int posx = 256;
     int posy = 256;
 
-    // METODO CONSTRUCTOR, QUE COGE LAS CONDICIONES INICIALES Y LES DA VALORES
     public PortalJava() {
+        loadImages();
 
-        try {
-            URL resource = PortalJava.class.getResource("/PortalJava/images/players/player1.png");
-            imagenmanzana = ImageIO.read(resource);
+        player = new Player(600, 400);
 
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        frame = new JFrame("Lazzy Portals");
 
-        frame = new JFrame("Manzanajuego");                                   // Creamos una ventana
+        JPanel panel = (JPanel) frame.getContentPane();
+        panel.setPreferredSize(new Dimension(512, 512));
+        panel.setLayout(null);
+        panel.setFocusable(true);
 
-        JPanel panel = (JPanel) frame.getContentPane();                         // Creamos un nuevo panel dentro del marco
-        panel.setPreferredSize(new Dimension(512, 512));                        // Introducimos propiedades del panel
-        panel.setLayout(null);                                                  // Inicializamos el layout
-        panel.setFocusable(true);                                               // Permitimos que podamos hacer focus
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(true);
+        frame.setVisible(true);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);                   // Cerrar el proceso al salir del programa
-        frame.pack();                                                           // Empaquetamos
-        frame.setResizable(true);                                              // No permitimos el reescalado
-        frame.setVisible(true);                                                 // Hacemos que sea visible
+        //Game Canvas
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, 512, 512);
+        canvas.setIgnoreRepaint(true);
+        canvas.setVisible(true);
+        panel.add(canvas);
 
-        //Crear Canvas
-        canvas = new Canvas();                                                  // Creamos un nuevo canvas
-        canvas.setBounds(0, 0, 512, 512);                                  // Especificamos las proporciones del canvas
-        canvas.setIgnoreRepaint(true);                                        // Ignoramos la repintura
-        panel.add(canvas);                                                      // Añadimos el canvas al panel
+        //Interaction Canvas
+        interactionCanvas = new Canvas();
+        interactionCanvas.setBounds(0, 0, 512, 512);
+        interactionCanvas.setIgnoreRepaint(true);
+        interactionCanvas.setVisible(false);
+        panel.add(interactionCanvas);
 
-        canvas.createBufferStrategy(2);                                         // Creamos una estrategia de buffer
-        bufferStrategy = canvas.getBufferStrategy();                            // Asignamos al canvas
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
         canvas.requestFocus();
 
+        interactionCanvas.createBufferStrategy(2);
+        interactionCanvas.requestFocus();
+
+    }
+
+    private void loadImages() {
+        closeDoorImageV = getImage("closedDoorV.png");
+        openDoorImageV = getImage("openDoorV.png");
+        switchOffImage = getImage("deactivatedButton.png");
+        switchOnImage = getImage("activeButton.png");
+        imgMap = getImage("map_1.png");
+        imgMapCollisions = getImage("map_color_1.png");
+    }
+
+    private BufferedImage getImage(String imageName) {
+        BufferedImage image = null;
+        try {
+            URL resource = PortalJava.class.getResource("/PortalJava/images/" + imageName);
+            image = ImageIO.read(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 
     // RALMENTE RECORDAMOS QUE EN JAVA EL QUE REALMENTE EJECUTA LAS COSAS ES EL MAIN
